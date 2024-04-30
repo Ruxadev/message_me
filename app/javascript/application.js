0c23456eb3cb209a98043b3c59315f8dc8b5501f
Ruxadev <grmdev01@gmail.com>
2024-04-30 00:17:00 -0300

    Enable dropdown functionality and crete nav partial

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,15 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import 'semantic-ui-sass'
-import 'semantic-ui-sass/semantic-ui'
-//= require jquery
-//= require jquery_ujs
-//= require rails ujs
-//= require activestorage
-//= require turbolinks
-//= require_tree .
-document.addEventListener('DOMContentLoaded', () => {
-    $('.ui.dropdown').dropdown();
-    // Add more Semantic UI component initializations as needed
-  });
+ 
+import "jquery"
+import "semantic-ui"
+ 
+$(document).on('turbo:load', function() {
+  $('.ui.dropdown').dropdown();
+})
