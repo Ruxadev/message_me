a75dd26c905ab9c1b0d5bd2da4aceb0151d5493e
Ruxadev <grmdev01@gmail.com>
2024-05-07 20:35:43 -0300

    Add auto-scrolling, Beautify input box, functionality and create custom scope

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,11 +2,30 @@
 //= require jquery_ujs
 import "@hotwired/turbo-rails"
 import "controllers"
+import "jquery"
 import "semantic-ui"
 import "channels"
+
+$(document).on('turbo:load', function() {
+  if ($('#messages').length > 0) {
+  $('#messages').scrollTop($('#messages')[0].scrollHeight);
+  }
+})
+
+$(document).on('turbo:load', function() {
+  $('#message_body').on('keydown', function(e) {
+    if (e.keyCode == 13) {
+      $('button').click();
+      $('#message_body').val('');
+    }
+  })
+})
+
 $(document).on('turbo:load', function () {
   $('.ui.dropdown').dropdown();
   $('.message .close').on('click', function () {
       $(this).closest('.message').transition('fade');
     });
+  submit_message();
+  scroll_bottom();
 })
